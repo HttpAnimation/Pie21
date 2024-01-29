ecc00ec2cf4ea7280830cae83c191f31a8f956c9
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-01-28 18:13:34 -0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
+const path = require('path');
+
 const app = express();
 const port = 9532;
 
 app.use(cors());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello, this is Pie21 proxy server!');
@@ -25,7 +30,11 @@ app.get('/e621', async (req, res) => {
   }
 });
 
+// For any other route, serve the 'index.html' file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(port, () => {
-  console.log(`Proxy server listening at http://localhost:${port}`);
-  console.log(`Localhost will be soon lost so don't bookmark that tab a ip based system will be put in place later :)`)
+  console.log(`Server listening at http://localhost:${port}`);
 });
