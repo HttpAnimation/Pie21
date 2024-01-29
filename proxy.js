982d3ce792ab78b574bb32f4771a1e214dc987c6
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-01-28 22:04:13 -0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -6,14 +6,12 @@ const path = require('path');
 const app = express();
 const port = 9532;
 
-// Allow requests from any origin
 app.use(cors({
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   headers: 'Content-Type',
 }));
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -35,7 +33,6 @@ app.get('/e621', async (req, res) => {
   }
 });
 
-// For any other route, serve the 'index.html' file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
